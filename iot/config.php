24b3118f0052b4dcd1bc943eaef525b82ba5b2c1
brifer <32596471+brifer@users.noreply.github.com>
2022-02-20 22:11:45 -0600

    Add files via upload

---
+++ iot/config.php
@@ -0,0 +1,9 @@
+<?php
+class Config{
+    
+    const SECRET = "loquesea";
+    const HOST = "localhost";
+    const USER = "root";
+    const BD = "iot";
+    const PASS = "";
+}
---
+++ iot/test.php
@@ -0,0 +1,5 @@
+<?php
+$sensor = $_REQUEST["sensor"];
+$valor = $_REQUEST["valor"];
+
+echo "El valor de $sensor es $valor";
